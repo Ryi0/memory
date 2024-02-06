590a8a85dee109f814222e5c4872263fde84e333
ryi0 <wpaquette455@gmail.com>
2024-02-06 13:37:43 -0500

    This workin fr fr fr

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styleUrl: './square.component.css'
+})
+export class SquareComponent {
+
+}
